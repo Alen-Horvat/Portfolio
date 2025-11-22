b3185742cb1aa6f3186966e2b3d8c71cd364dfcc
Alen Horvat <W0865240@myscc.ca>
2025-11-22 11:25:32 -0500

    Added formatting for the java blackjack project

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {MatDivider} from '@angular/material/divider';
 import {MatTab, MatTabGroup} from '@angular/material/tabs';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatDivider, MatTabGroup, MatTab],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatDivider, MatTabGroup, MatTab, NgOptimizedImage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
